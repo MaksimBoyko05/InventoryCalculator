55c618d40081a7fecb6887f056e73f6cc37d57f2
MaksimBoyko05 <vladosmakasin1@gmail.com>
2026-05-11 16:27:43 +0300

    Started working on education project.

--- src/Inventory/components/ResourceCard.jsx
+++ src/Inventory/components/ResourceCard.jsx
@@ -1,23 +1,24 @@
 import styles from "../inventory.module.css"
-export default function ResourceCard({resources}){
-  return(
+
+export default function ResourceCard({resources}) {
+  return (
     <>
       <div className={styles.cardscontainer}>
         {resources.map(resource => (
-        <div
-          className={styles.itemcard}
-          key={resource.id}>
-          <div className={styles.itemtitle}>
-            <span>{resource.icon}</span>
-            <p>{resource.name}</p>
+          <div
+            className={styles.itemcard}
+            key={resource.id}>
+            <div className={styles.itemtitle}>
+              <span>{resource.icon}</span>
+              <p>{resource.name}</p>
+            </div>
+            <p className={styles.itemquantity}>{resource.quantity}</p>
+            <div className={styles.btncontainer}>
+              <button disabled={resource.quantity < 1}>-</button>
+              <button>+</button>
+            </div>
           </div>
-          <p className={styles.itemquantity}>{resource.quantity}</p>
-          <div className={styles.btncontainer}>
-            <button  disabled={resource.quantity < 1}>-</button>
-            <button>+</button>
-          </div>
-        </div>
-      ))}
+        ))}
       </div>
     </>
   )
